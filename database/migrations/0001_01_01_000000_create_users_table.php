708bff45e012a9387b531cbd1ede0998f73e3a42
Sahiru <sahiru906@gmail.com>
2024-09-05 18:49:09 +0530

    updated

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,12 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user', function (Blueprint $table) {
             $table->id();
+            $table->integer('profile_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('shop_id');
+            $table->string('role');
             $table->rememberToken();
             $table->timestamps();
         });
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\ShopController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/addshop', [ShopController::class, 'addshop']);
+Route::post('/submitshop', [ShopController::class, 'submitshop']);
+Route::put('/shop/update/{id}', [ShopController::class, 'updateShop']);
+
