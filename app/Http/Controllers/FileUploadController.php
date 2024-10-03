91ab60b0731b95c89f29ca561be2a10172823956
udara lakshitha <121849195+UDARALAKSHITHA23@users.noreply.github.com>
2024-10-03 17:31:54 +0530

    fbddhh

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -6,6 +6,30 @@ use Illuminate\Http\Request;
 
 class FileUploadController extends Controller
 {
+    // public function upload(Request $request)
+    // {
+    //     // Validate the uploaded files for each section
+    //     $request->validate([
+    //         'left_image.*' => 'file|mimes:jpg,png,jpeg|max:2048',
+    //         'middle_image.*' => 'file|mimes:jpg,png,jpeg|max:2048',
+    //         'right_image.*' => 'file|mimes:jpg,png,jpeg|max:2048',
+    //     ]);
+
+    //     $uploadedFiles = [];
+
+    //     foreach (['left_image', 'middle_image', 'right_image'] as $imageType) {
+    //         if ($request->hasFile($imageType)) {
+    //             foreach ($request->file($imageType) as $file) {
+    //                 $filename = time() . '_' . $file->getClientOriginalName();
+    //                 $file->move(public_path('assets/Advertisementuploads'), $filename);
+    //                 $uploadedFiles[] = $filename;
+    //             }
+    //         }
+    //     }
+
+    //     return response()->json(['success' => true, 'message' => 'Files uploaded successfully.', 'files' => $uploadedFiles]);
+    // }
+
     public function upload(Request $request)
     {
         // Validate the uploaded files for each section
@@ -20,7 +44,11 @@ class FileUploadController extends Controller
         foreach (['left_image', 'middle_image', 'right_image'] as $imageType) {
             if ($request->hasFile($imageType)) {
                 foreach ($request->file($imageType) as $file) {
-                    $filename = time() . '_' . $file->getClientOriginalName();
+                    // Generate a unique filename using current date and time
+                    $timestamp = now()->format('Ymd_His'); // Format: YYYYMMDD_HHMMSS
+                    $filename = "{$imageType}_{$timestamp}_" . $file->getClientOriginalName();
+
+                    // Move the file to the desired directory
                     $file->move(public_path('assets/Advertisementuploads'), $filename);
                     $uploadedFiles[] = $filename;
                 }
@@ -29,5 +57,4 @@ class FileUploadController extends Controller
 
         return response()->json(['success' => true, 'message' => 'Files uploaded successfully.', 'files' => $uploadedFiles]);
     }
-
 }
