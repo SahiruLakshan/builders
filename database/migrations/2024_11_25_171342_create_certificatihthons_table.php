c1663823fecbc4d7c5d53a6270788ba48c7d12c0
udara lakshitha <121849195+UDARALAKSHITHA23@users.noreply.github.com>
2024-11-28 12:56:31 +0530

    sdf

--- database/migrations/2024_11_25_171342_create_certificatihthons_table.php
+++ database/migrations/2024_11_25_171342_create_certificatihthons_table.php
@@ -5,29 +5,45 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
+    public function up()
+    {
+        if (!Schema::hasTable('certifications')) {
+            Schema::create('certifications', function (Blueprint $table) {
+                $table->id();
+                $table->unsignedBigInteger('professional_id');
+                $table->string('certification_name');
+                $table->string('issuing_authority');
+                $table->date('date_issued')->nullable();
+                $table->date('expiration_date')->nullable();
+                $table->string('certification_pdf')->nullable();
+                $table->timestamps();
+            });
+        }
+    }
+
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('certifications', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('professional_id')->constrained()->onDelete('cascade');
-            $table->string('certification_name');
-            $table->string('issuing_authority');
-            $table->date('date_issued')->nullable();
-            $table->date('expiration_date')->nullable();
-            $table->string('certification_pdf')->nullable();
-            $table->timestamps();
-        });
+    // public function up(): void
+    // {
+    //     Schema::create('certifications', function (Blueprint $table) {
+    //         $table->id();
+    //         $table->foreignId('professional_id')->constrained()->onDelete('cascade');
+    //         $table->string('certification_name');
+    //         $table->string('issuing_authority');
+    //         $table->date('date_issued')->nullable();
+    //         $table->date('expiration_date')->nullable();
+    //         $table->string('certification_pdf')->nullable();
+    //         $table->timestamps();
+    //     });
 
-    }
+    // }
 
     /**
      * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('certifications');
-    }
+    //  */
+    // public function down(): void
+    // {
+    //     Schema::dropIfExists('certifications');
+    // }
 };
