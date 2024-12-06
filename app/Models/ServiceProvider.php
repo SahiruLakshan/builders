ae893e8ed06e84ae5baa7dc94a24151b7068e109
udara lakshitha <121849195+UDARALAKSHITHA23@users.noreply.github.com>
2024-12-06 21:25:00 +0530

    Update ServiceProvider.php

--- app/Models/ServiceProvider.php
+++ app/Models/ServiceProvider.php
@@ -5,18 +5,36 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Category;
+use App\Models\City;
+use App\Models\District;
+use App\Models\Service;
 
 class ServiceProvider extends Model
 {
     use HasFactory;
 
 
-    protected $table='service_providers';
+    protected $table = 'service_providers';
 
     protected $fillable = [
-        'number','s_name', 'grade', 'address', 'category_id', 'district_id','longitude', 'latitude', 'city_id', 
-        'telephone', 'mobile', 'whatsapp', 'company_name', 'business_reg_no', 
-        'no_of_employees', 'employees_qualification', 'max_project_value','status'
+        'number',
+        's_name',
+        'grade',
+        'address',
+        'category_id',
+        'district_id',
+        'longitude',
+        'latitude',
+        'city_id',
+        'telephone',
+        'mobile',
+        'whatsapp',
+        'company_name',
+        'business_reg_no',
+        'no_of_employees',
+        'employees_qualification',
+        'max_project_value',
+        'status'
     ];
 
     public function district()
