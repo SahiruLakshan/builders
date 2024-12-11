364783f2edbcfce338e146c18860656b1979b847
udara lakshitha <121849195+UDARALAKSHITHA23@users.noreply.github.com>
2024-12-11 11:39:15 +0530

    add

--- resources/views/webpages/serviceproviderform.blade.php
+++ resources/views/webpages/serviceproviderform.blade.php
@@ -45,6 +45,13 @@
                             placeholder="Name Of The Service Provider" />
                     </div>
                     <div class="col">
+                        <div class="form-group">
+                            <label for="inputPassword4">Email</label>
+                            <input type="email" class="form-control" id="email" name="email"
+                                placeholder="Email Of The Service Provider" />
+                        </div>
+                    </div>
+                    <div class="col">
                         <label for="maxProjectValue">Grade </label>
                         <input type="text" class="form-control" id="grade" name="grade"
                             placeholder="Grade Of The Service Provider" />
